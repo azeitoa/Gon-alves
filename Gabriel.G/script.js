7c2a7004ef2bac00b21d9be95c5b2b8cac9ef11f
azeitoa <gabrielanjos620@gmail.com>
2024-12-09 17:27:06 -0300

    Update script.js

---
+++ Gabriel.G/script.js
@@ -0,0 +1,102 @@
+
+
+
+
+
+body {
+  color: purple;
+ background: black;
+ margin: 10px 10px 10px 10px;
+ font-family: "Hachi Maru Pop", cursive;
+ font-weight: 400;
+ font-style: normal;
+ margin-bottom: 100px;
+
+}
+
+header {
+  border-bottom: solid 2px rgb(42, 122, 228);
+  padding: 20px;
+  font-size: 32px;
+  color: rgb(42, 122, 228);
+}
+section {
+  background: rgb(184,156,213);
+  padding-bottom: 80px;
+  padding-top: 80px;
+  display: flex;
+  justify-content: center;
+}
+div {
+margin-right: 20px;
+
+}
+
+h1 {
+margin: 0px 0px 0px 50px;
+
+}
+p {
+padding: 20px;
+font-size: 32px;
+margin: 0px 0px 0px 50px;
+
+}
+.chamada-texto {
+margin-right:  20px;
+
+}
+
+img {
+height: 200px;
+}
+.categoria-videos {
+display: flex;
+}
+
+section {
+background: rgb(184, 156, 213);
+padding-bottom: 80px;
+padding-top: 80px;
+display: flex;
+justify-content: center;
+}
+
+.categoria-videos {
+display: flex;
+overflow-x: auto;
+gap: 10px;
+}
+
+section {
+padding-left: 20px;
+padding-right: 20px;
+}
+.chamada {
+background: rgb(184, 156, 213);
+padding-bottom: 80px;
+padding-top: 80px;
+display: flex;
+justify-content: center;
+}
+
+
+.categoria {
+padding-left: 20px;
+padding-right: 20px;
+margin-top: 50px;
+}
+
+.categoria-videos img {
+opacity: 0.5;
+height: 200px;
+}
+
+.categoria-videos img:hover {
+opacity: 1.0;
+border: 1px solid green;
+}
+
+.categoria h2 {
+color: rgb(42, 122, 228);
+}
