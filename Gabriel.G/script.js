f3f6d123d3d40f202f7b5cce4a02d77667f21a30
azeitoa <gabrielanjos620@gmail.com>
2024-12-10 19:01:46 -0300

    Update script.js

--- Gabriel.G/script.js
+++ Gabriel.G/script.js
@@ -1,102 +1,115 @@
 
 
-
-
-
 body {
-  color: purple;
- background: black;
- margin: 10px 10px 10px 10px;
- font-family: "Hachi Maru Pop", cursive;
- font-weight: 400;
- font-style: normal;
- margin-bottom: 100px;
+    color: purple;
+   background: black;
+   margin: 10px 10px 10px 10px;
+   font-family: "Hachi Maru Pop", cursive;
+   font-weight: 400;
+   font-style: normal;
+   margin-bottom: 100px;
 
-}
+   font-family: "Roboto Condensed", sans-serif;
+   font-optical-sizing: auto;
+   font-style: normal;
+ }
 
-header {
-  border-bottom: solid 2px rgb(42, 122, 228);
-  padding: 20px;
-  font-size: 32px;
-  color: rgb(42, 122, 228);
+  header {
+    border-bottom: solid 2px rgb(42, 122, 228);
+    padding: 20px;
+    font-size: 32px;
+    color: rgb(42, 122, 228);
 }
 section {
-  background: rgb(184,156,213);
+    background: rgb(184,156,213);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+}
+div {
+  margin-right: 20px;
+
+}
+
+h1 {
+  margin: 0px 0px 0px 50px;
+
+}
+p {
+  padding: 20px;
+  font-size: 32px;
+  margin: 0px 0px 0px 50px;
+
+}
+.chamada-texto {
+  margin-right:  20px;
+
+}
+
+img {
+  height: 200px;
+  max-width: 90%;
+}
+.categoria-videos {
+  display: flex;
+}
+
+section {
+  background: rgb(184, 156, 213);
   padding-bottom: 80px;
   padding-top: 80px;
   display: flex;
   justify-content: center;
 }
-div {
-margin-right: 20px;
 
-}
-
-h1 {
-margin: 0px 0px 0px 50px;
-
-}
-p {
-padding: 20px;
-font-size: 32px;
-margin: 0px 0px 0px 50px;
-
-}
-.chamada-texto {
-margin-right:  20px;
-
-}
-
-img {
-height: 200px;
-}
 .categoria-videos {
-display: flex;
+  display: flex;
+  overflow-x: auto;
+  gap: 10px;
 }
 
 section {
-background: rgb(184, 156, 213);
-padding-bottom: 80px;
-padding-top: 80px;
-display: flex;
-justify-content: center;
-}
-
-.categoria-videos {
-display: flex;
-overflow-x: auto;
-gap: 10px;
-}
-
-section {
-padding-left: 20px;
-padding-right: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 .chamada {
-background: rgb(184, 156, 213);
-padding-bottom: 80px;
-padding-top: 80px;
-display: flex;
-justify-content: center;
+  background: rgb(184, 156, 213);
+  padding-bottom: 80px;
+  padding-top: 80px;
+  display: flex;
+  justify-content: center;
 }
 
 
 .categoria {
-padding-left: 20px;
-padding-right: 20px;
-margin-top: 50px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 50px;
 }
 
 .categoria-videos img {
-opacity: 0.5;
-height: 200px;
+  opacity: 0.5;
+  height: 200px;
 }
 
 .categoria-videos img:hover {
-opacity: 1.0;
-border: 1px solid green;
+  opacity: 1.0;
+  border: 1px solid green;
 }
 
 .categoria h2 {
-color: rgb(42, 122, 228);
+  color: rgb(42, 122, 228);
+}
+
+.passo {
+  display: none;
+}
+
+.passo.ativo {
+  display:block;
+}
+main {
+  text-align: center;
+  max-width: 90%;
 }
